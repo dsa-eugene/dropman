29a312905e2ac3b738b64627d73f66e800ad27be
adrsch <35346279+adrsch@users.noreply.github.com>
2020-05-13 11:59:21 -0700

    Improve user experience in Meet interface

--- wp-dropman/inc/admin_panel.php
+++ wp-dropman/inc/admin_panel.php
@@ -1,7 +1,9 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) { die; } // do not run directly
 
-//TODO: make it so its not broken
-//require_once DROPMAN_DIR . 'dropman/index.html';
-//until then, a link will suffice
-echo '<div class="wrap"><a href="' . DROPMAN_URL . 'dropman/index.html"><h1> CLICK HERE TO GO TO CONTROL PANEL</h1></a></div>';
+//TODO: Set height dynamically: currently, the 700px comes from just looking at the height of it & plugging that in! Not the best...
+?>
+<div class="wrap">
+	<iframe src="<?php echo DROPMAN_URL; ?>dropman/index.html" style="width: 100%; height: 700px;"></iframe>
+	<p>Note: If it gets stuck at "Loading data...", refresh the page and try again.</p>
+</div>
--- wp-dropman/inc/panel.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( ! defined( 'ABSPATH' ) ) { die; } // do not run directly
-
-//TODO: make control panel look like it's actually supposed to be there
-require_once DROPMAN_DIR . 'dropman/index.html';
